f9596ab210451d6d89f28b84a4bf929bd051526f
bahadirmemis <sadikbahadir.memis@migrosonline.com>
2024-03-10 14:32:33 +0300

    Add handleMethodArgumentNotValid

--- src/main/java/com/bahadirmemis/n11bootcamp2/general/GeneralControllerAdvice.java
+++ src/main/java/com/bahadirmemis/n11bootcamp2/general/GeneralControllerAdvice.java
@@ -2,10 +2,16 @@ package com.bahadirmemis.n11bootcamp2.general;
 
 import com.bahadirmemis.n11bootcamp2.exceptions.ItemNotFoundException;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +27,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GeneralControllerAdvice extends ResponseEntityExceptionHandler {
 
   private final KafkaProducerService kafkaProducerService;
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                                HttpStatusCode status, WebRequest request) {
+    List<Map<String, String>> errorList = ex.getBindingResult().getFieldErrors().stream()
+                                            .map(fieldError ->
+                                                 {
+                                                   Map<String, String> errorMap = new HashMap<>();
+                                                   errorMap.put(fieldError.getField(),
+                                                                fieldError.getDefaultMessage());
+                                                   return errorMap;
+                                                 }).toList();
+
+    String description = request.getDescription(false);
+
+    var generalErrorMessages = new GeneralErrorMessages(LocalDateTime.now(), errorList.toString(), description);
+    var restResponse = RestResponse.error(generalErrorMessages);
+
+    return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 
   @ExceptionHandler
   public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
